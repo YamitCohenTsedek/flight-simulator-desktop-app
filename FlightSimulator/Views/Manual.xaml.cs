93756e2f0bc6144954babd0d3f74b922866a5082
yamit.ct <yamit.ct@gmail.com>
2019-04-24 19:49:24 +0300

    Adding Manual View and AutoPilot View

---
+++ FlightSimulator/Views/Manual.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulator.Views
+{
+    /// <summary>
+    /// Interaction logic for Manual.xaml
+    /// </summary>
+    public partial class Manual : UserControl
+    {
+        public Manual()
+        {
+            InitializeComponent();
+        }
+
+        private void ThrottleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Joystick_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Rudder_bar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Throttle_bar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
